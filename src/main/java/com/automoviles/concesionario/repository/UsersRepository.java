24a5b0c4af297119429f3536c0462b66389acf6d
jorgemcroa228 <jorgemcroa228@gmail.com>
2026-03-05 00:47:23 -0500

    Creating UsersRepository with all Queries

--- src/main/java/com/automoviles/concesionario/repository/UsersRepository.java
+++ src/main/java/com/automoviles/concesionario/repository/UsersRepository.java
@@ -1,5 +1,7 @@
 package com.automoviles.concesionario.repository;
 
 public class UsersRepository {
-  
+  public static final String INSERT_USER = "INSERT INTO user (name, email, phone, address) VALUES (?,?,?,?)";
+
+  public static final String GET_USERS = "SELECT * FROM users";
 }
